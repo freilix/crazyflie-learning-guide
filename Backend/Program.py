0576504d69f20851d0eae51e7e134fe80f8334ff
freilix <31540389+freilix@users.noreply.github.com>
2017-11-22 16:19:03 +0100

    small changes

--- Backend/Program.py
+++ Backend/Program.py
@@ -8,18 +8,15 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 from Backend import SequenceList
 
-uri = 'radio://0/80/2M' #TODO
+uri = 'radio://0/80/250K' #TODO available = cflib.crtp.scan_interfaces()
 sequenceList = SequenceList()
-
-def reset_estimator(scf):
-    cf = scf.cf
 
 def run_sequence(scf, sequence):
     cf = scf.cf
     sList = sequenceList.getSequence()
+    for element in sList:
+        element.run(cf)
 
 cflib.crtp.init_drivers(enable_debug_driver=False)
-#TODO available = cflib.crtp.scan_interfaces()
 with SyncCrazyflie(uri) as scf:
-    reset_estimator(scf)
     run_sequence(scf, sequenceList)
