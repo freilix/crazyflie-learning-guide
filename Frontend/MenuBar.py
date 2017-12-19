a0ef806e058bd4f285b7299c31ef5f95d142477c
freilix <31540389+freilix@users.noreply.github.com>
2017-12-19 10:44:27 +0100

    added layout to MenuBar

--- Frontend/MenuBar.py
+++ Frontend/MenuBar.py
@@ -1,10 +1,16 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QBrush, QColor
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton
 
 
 class MenuBar(QWidget):
     def __init__(self):
         super().__init__()
         self.setFixedHeight(30)
+        self.layout = QHBoxLayout()
+
+        self.layout.addWidget(QLabel("Test1"))
+        #self.layout.addWidget(QPushButton("Test"))
+
+        self.setLayout(self.layout)
 
